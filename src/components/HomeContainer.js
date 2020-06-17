1a5edb5b0d7af174bf01744bc1883fe3dd34dd4c
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-17 13:59:58 +0200

    HomeContainer.js => Modificacion de la url a traves del elemento history, que es proporcionado por el withRouter

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+//Quitamos el Link que ya no lo estabamos usando y metemos el withRouter
+import { withRouter } from "react-router-dom";
 import AppFrame from "./../components/AppFrame";
 import CustomerActions from "./../components/CustomersActions";
 
 class HomeContainer extends Component {
   handleOnClick = () => {
     console.log("handleOnClick");
+
+    //Para continuar y hacer la conexión atraves del boton utilizaremos el elemento history que nos provee react-router-dom
+    //Solo podemos acceder a este elemento mientras nuestro componente este dentro de el Router y como a HomerContainer lo estan llamando en el App
+    //mediante un Router no tenemos problema.
+    //history se maneja al estilo stag, quiere decir que si queremos navegar a otra URL ara un push y se agregará un elemento a esta y despues si
+    //hacemos un back o el usuario hace un back el elemento desaparece y volvemos a la pagina de home
+    this.props.history.push("/customers");
   };
   render() {
     return (
@@ -31,4 +39,7 @@ class HomeContainer extends Component {
 
 HomeContainer.propTypes = {};
 
-export default HomeContainer;
+//Función withRouter recibe como parametro nuestro componente y retorna otro componente decorado, es decir que le agrega funcionalidad
+//Agregandole las propiedades de => history, location, math
+//Por lo tanto una forma de que funcione siempre independientemente de como llamemos al componente es utilizando esta función:
+export default withRouter(HomeContainer);
