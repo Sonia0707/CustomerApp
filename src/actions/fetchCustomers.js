45afcd6fcf166be1771ef099bd3f4ddf02809625
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-18 13:17:31 +0200

    fetchCustomers => Punto de interrucción, para comprobar si se ejecuta en el componentDidMount

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,4 +1,6 @@
 export const fetchCustomers = () => {
+  //Como aún no tenemos la herramienta de redux activada, vamos a poner un punto de interrucción para
+  //comprobar si la action se esta ejecutando el componentDidMount...
   debugger;
   return {
     type: "FETCH_CUSTOMERS",
