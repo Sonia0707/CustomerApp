19d7ded0f799bf4001486f9e50e208a0f64dc735
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-19 10:15:13 +0200

    Cambiamos el STORE para añadir la herramiente de REDUX_DEVTOOLS.

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
+
+//Creamos la herramienta de REDUX_DEVTOOLS para poder visualizar mejor el state de nuestra aplicación:
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //Generamos una constante de reducers para pasar demomento una constante
 //con el parametro de state y retorna un state
 const reducers = (state) => state;
-//Recoge los recucers y como segundo parametro espera un estado, así que le pasamos un estado enti por el momento:
-export const store = createStore(reducers, {});
+//Recoge los recucers, como segundo parametro espera un estado y el tercero en nuestra herramienta para redux en chrome:
+export const store = createStore(reducers, {}, composeEnhancers());
