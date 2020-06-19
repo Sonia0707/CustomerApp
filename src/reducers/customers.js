4690cc6acb12d3e056444f21e1a373b2471efe1c
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-19 11:22:27 +0200

    customers.js REDUCERS => Ponemos el 2º párametro que es la accion del payload (la lista de clientes de las actions). Haciendo una copia.

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -5,10 +5,10 @@ import { FETCH_CUSTOMERS } from "../constantes";
 //Generar constante que se iguala al resultado de  handleActions
 //Que esta esperando un objeto, y este objeto va estar compuesto de las distintas constantes como claves, de este diccionario
 // y luego va estar asociada a una funcion. Podemos tener un listado de Acciones, por el momento lo dejamos así.
-//Segundo párametro valor por defecto que va a ser el valor inicial de nuestro state.
+//Segundo párametro valor inicial del state y creamos una copia con el state opereitor. (Necesita más refactor)
 export const customers = handleActions(
   {
-    [FETCH_CUSTOMERS]: (state) => state,
+    [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
   },
-  {}
+  []
 );
