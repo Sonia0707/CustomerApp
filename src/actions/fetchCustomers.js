bc2b2b90e42d0e348bbae5058d6411924e9898a8
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-19 11:17:40 +0200

    fetchCustomers.js => (payload), segundo parametro que necesita la accion.

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -2,6 +2,25 @@ import { FETCH_CUSTOMERS } from "./../constantes";
 //Importamos el create:
 import { createAction } from "redux-actions";
 
+//Creamos aqui la lista por defecto de clientes: (payload), segundo parametro que necesita la accion:
+const customers = [
+  {
+    dni: "71462879q",
+    name: "Sonia Vidal",
+    age: 28,
+  },
+  {
+    dni: "78953647w",
+    name: "Jose Gonzalez",
+    age: 37,
+  },
+  {
+    dni: "09876543e",
+    name: "Luis Martinez",
+    age: 25,
+  },
+];
+
 //Le pasamos directamente la constante que hemos generado y de momento el payload,
 //se lo quitamos ya que esta vacio:
-export const fetchCustomers = createAction(FETCH_CUSTOMERS);
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers);
