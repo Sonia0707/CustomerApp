e4d0e07a79705cbf4f1c0b832f4f98e5bc8a6979
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-19 10:57:40 +0200

    index.js del STORE,  quitamos la constante ahora ya podemos llamar a los reducers creados.

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore, compose } from "redux";
+//Vamos a invocar los reducers:
+import reducers from "./../reducers";
 
 //Creamos la herramienta de REDUX_DEVTOOLS para poder visualizar mejor el state de nuestra aplicación:
 
@@ -6,6 +8,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //Generamos una constante de reducers para pasar demomento una constante
 //con el parametro de state y retorna un state
-const reducers = (state) => state;
+//const reducers = (state) => state; ¡¡¡Eliminamos constante solo la teniamos de prueba hasta pasarle el reducers!!!
+
 //Recoge los recucers, como segundo parametro espera un estado y el tercero en nuestra herramienta para redux en chrome:
 export const store = createStore(reducers, {}, composeEnhancers());
