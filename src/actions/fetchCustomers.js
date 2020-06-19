8e41e933016928f41b662879c5a2d6bf7d458040
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-19 10:22:27 +0200

    fetchCustomers => Cambio, llamamos a la constante y eso es lo que le pasamos al createAction que hemos importado de redux-action

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,9 +1,7 @@
-export const fetchCustomers = () => {
-  //Como aún no tenemos la herramienta de redux activada, vamos a poner un punto de interrucción para
-  //comprobar si la action se esta ejecutando el componentDidMount...
-  debugger;
-  return {
-    type: "FETCH_CUSTOMERS",
-    payload: null,
-  };
-};
+import { FETCH_CUSTOMERS } from "./../constantes";
+//Importamos el create:
+import { createAction } from "redux-actions";
+
+//Le pasamos directamente la constante que hemos generado y de momento el payload,
+//se lo quitamos ya que esta vacio:
+export const fetchCustomers = createAction(FETCH_CUSTOMERS);
