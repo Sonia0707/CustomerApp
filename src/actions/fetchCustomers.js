a145df8707b90f7030f3d7539c11694d14355e12
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-22 10:01:51 +0200

    fetchCustomers.js => Borramos los datos, porque ahora los vamos a recibir del servidor, metemos la URL que hemos levantado con el servidor en una constante y se la pasamos a la promise haciendo el fetch para coger los datos. Y se lo pasamos en la exportación.

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -2,25 +2,11 @@ import { FETCH_CUSTOMERS } from "./../constantes";
 //Importamos el create:
 import { createAction } from "redux-actions";
 
-//Creamos aqui la lista por defecto de clientes: (payload), segundo parametro que necesita la accion:
-const customers = [
-  {
-    dni: "71462879q",
-    name: "Sonia Vidal",
-    age: 28,
-  },
-  {
-    dni: "78953647w",
-    name: "Jose Gonzalez",
-    age: 37,
-  },
-  {
-    dni: "09876543e",
-    name: "Luis Martinez",
-    age: 25,
-  },
-];
+//Url levantada por el servidor
+const url = "http://localhost:3001/customers";
 
-//Le pasamos directamente la constante que hemos generado y de momento el payload,
-//se lo quitamos ya que esta vacio:
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers);
+//La funcion que contiene una promise:
+const apiFetchCustomers = () => fetch(url).then((v) => v.json());
+
+//Le pasamos directamente la constante que hemos generado y la función que contiene la promise:
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
