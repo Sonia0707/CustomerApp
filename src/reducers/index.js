294e133eb5359f0334de7afe2aaa8ff3a7e87e4e
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-23 11:41:20 +0200

    index.js REDUCERS => generamos un reducers nuevo con el redux-form poniendole la kit correspondiente.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 import { customers } from "./customers"; //Importamos el reducers.
+//Creamos el reducer de form: (Alias para que no sea un nombre muy generico)
+import { reducer as reduxForm } from "redux-form";
 
 //Creamos la llamada a los reducers con combineReducers:
+//Ponemos la kit de redux-form que es la que esta esperando: form: (es la recomendada)
 export default combineReducers({
   customers,
+  form: reduxForm,
 });
