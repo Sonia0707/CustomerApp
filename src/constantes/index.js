b1fe1293b49fdbe1c7b2055be8ace93de8509598
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-25 13:25:01 +0200

    Constante UPDATE_CUSTOMER=> Creamos constante para poder hacer los cambios del JSON

--- src/constantes/index.js
+++ src/constantes/index.js
@@ -1,2 +1,4 @@
 //Creando constante para la accion fecthCustomers:
 export const FETCH_CUSTOMERS = "FETCH_CUSTOMERS";
+//Creamos constante para poder hacer los cambios del JSON:
+export const UPDATE_CUSTOMER = "UPDATE_CUSTOMER";
