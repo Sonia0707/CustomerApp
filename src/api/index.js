561823cb0d40451142253bcf30b470d04ce8927a
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-25 13:24:12 +0200

    apiPut => Creamos una nueva api donde haremos de nuevo una llamada con el fetch y le pasaremos los nuevos datos = "PUT" para ello recibe la url, id, obj. Una vez que lo tiene realiza el PUT.

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,10 @@
 //Recibe como parametro una URL y retorna una fución que eso es lo que esta esperando el payload creator:
 //Dentro de la función llama al fetch con la url que le pasemos como parametro y despues resuelve tomando el json en el promise:
 export const apiGet = (url) => () => fetch(url).then((v) => v.json());
+
+export const apiPut = (url, id, obj) => () =>
+  fetch(`${url}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(obj),
+    headers: new Headers({ "Content-type": "application/json" }),
+  }).then((v) => v.json());
