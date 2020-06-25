4bd81a6d9fec3fb02e78dfaa94f5ec651cd74033
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-25 11:27:54 +0200

    CustomerEdit.js => Quitamos la función de isRequired para poner de forma golbal las validaciones con la función validate que se la pasamos al Higher Order Component.

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -5,7 +5,7 @@ import { reduxForm, Field } from "redux-form";
 import { setPropsAsInitial } from "./../helpers/setPropsAsInitial";
 
 //Funcion con unico parametro, en base a ese resultado retornamos un único resultado: VAMOS A CREAR VALIDACIONES A NIVEL DE FIELD:
-const isRequired = (value) => !value && "Este campo es requerido";
+//const isRequired = (value) => !value && "Este campo es requerido";
 
 //Genero componente para el input:
 // -(meta.error)= Mensaje que se manda cuando el valor es vacio en isRequired.
@@ -23,6 +23,21 @@ const MyField = ({ input, meta, type, label, name }) => (
 //Vammos a crear una función para asegurarnos de que lo que llega es un numero a nuestro campo:
 const isNumber = (value) => isNaN(Number(value)) && "Debe ser un numero";
 //Ponemos las validaciones (isRequired y isNumber) y el componente requerido (MyField):
+
+//Siguiente paso eliminamos isRequired ya que lo vamos hacer una Validación global:
+//Asi que creamos la función de validate que contiene varios valores, espera como retorno los posibles errores,
+//pero la diferencia es que los errores van estar representados como objetos, cuyas claves van a ser cada uno de los campos que hayan tenido error:
+const validate = (values) => {
+  const error = {};
+
+  if (!values.name) {
+    error.name = "El campo nombre es requerido";
+  }
+  if (!values.dni) {
+    error.dni = "El dni es un campo requerido";
+  }
+  return error;
+};
 const CustomerEdit = ({ name, dni, age }) => {
   return (
     <div>
@@ -32,18 +47,10 @@ const CustomerEdit = ({ name, dni, age }) => {
           name="name"
           component={MyField}
           type="text"
-          validate={isRequired}
           label="Nombre:"
         ></Field>
 
-        <Field
-          name="dni"
-          component={MyField}
-          type="text"
-          validate={(isRequired, isNumber)}
-          label="DNI:"
-        ></Field>
-
+        <Field name="dni" component={MyField} type="text" label="DNI:"></Field>
         <Field
           name="age"
           component={MyField}
@@ -66,7 +73,11 @@ CustomerEdit.propTypes = {
 //tiene que mantener el nombre ya en todo momento una vez puesto:
 
 //En este paso creamos una función con el decorado para hacerlo mas sencillo y pasarselo al Higher Order Component:
-const CustomersEditForm = reduxForm({ form: "CustomerEdit" })(CustomerEdit);
+// Utilizamos la funcion validate para crear las validaciones a nivel global:
+const CustomersEditForm = reduxForm({
+  form: "CustomerEdit",
+  validate,
+})(CustomerEdit);
 
 //Borramos el connect y utilizamos nuestro Higher Order Component (decoreitor) que establece las propiedades iniciales:
 export default setPropsAsInitial(CustomersEditForm);
