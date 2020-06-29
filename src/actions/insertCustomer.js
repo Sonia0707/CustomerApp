f61d681d12714cc9de559708d69d58b698ffed65
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-29 10:58:32 +0200

    INSERT_CUSTOMER=> Creamos nueva constante para hacer los nuevos clientes.

---
+++ src/actions/insertCustomer.js
@@ -0,0 +1,8 @@
+import { createAction } from "redux-actions";
+import { INSERT_CUSTOMER } from "../constantes";
+import { apiPost } from "../api";
+import { urlCustom } from "../api/urls";
+
+export const insertCustomer = createAction(INSERT_CUSTOMER, (customer) =>
+  apiPost(urlCustom, customer)()
+);
