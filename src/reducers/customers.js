0743b4f7bacd07562e78dd7301e090473b166a2f
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-29 11:31:38 +0200

    REDUCE= Función reduce de ES6. Sirve para no volver hacer el fecht de nuevo al hacer una modificación. Simplemente ve el array viejo y donde encuentra el cambio lo realiza. No busca más.

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,6 +1,10 @@
 //Es una utilidad que me permite manejar los reducers:
 import { handleActions } from "redux-actions"; //esto nos lo ofrece solo redux-actions
-import { FETCH_CUSTOMERS, INSERT_CUSTOMER } from "../constantes";
+import {
+  FETCH_CUSTOMERS,
+  INSERT_CUSTOMER,
+  UPDATE_CUSTOMER,
+} from "../constantes";
 
 //Generar constante que se iguala al resultado de  handleActions
 //Que esta esperando un objeto, y este objeto va estar compuesto de las distintas constantes como claves, de este diccionario
@@ -10,6 +14,24 @@ export const customers = handleActions(
   {
     [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
     [INSERT_CUSTOMER]: (state, action) => [...state, action.payload],
+    [UPDATE_CUSTOMER]: (state, action) => {
+      const customerPayload = action.payload;
+      const { id } = customerPayload; // i = 2 name= 'nuevo nombre'
+      // [{id: 1, name: '', ...},
+      // {id: 2, name: 'viejo nombre', ...},
+      // {id: 3, name: '', ...}]
+      const customers = state;
+      const initialValue = [];
+      // 1º Vacio [], luego cada valor:
+      const newCustomers = customers.reduce((acc, customer) => {
+        if (customer.id === id) {
+          return [...acc, customerPayload];
+        } else {
+          return [...acc, customer];
+        }
+      }, initialValue);
+      return newCustomers;
+    },
   },
   []
 );
