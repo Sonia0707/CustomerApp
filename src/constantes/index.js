a65d9e87e19fe79f41729ce107df981a56686e47
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-30 09:57:33 +0200

    Creamos la constate de: DELETE_CUSTOMER.

--- src/constantes/index.js
+++ src/constantes/index.js
@@ -2,5 +2,7 @@
 export const FETCH_CUSTOMERS = "FETCH_CUSTOMERS";
 //Creamos constante para poder hacer los cambios del JSON:
 export const UPDATE_CUSTOMER = "UPDATE_CUSTOMER";
-
+//Insertar nuevos clientes:
 export const INSERT_CUSTOMER = "INSERT_CUSTOMER";
+//Eliminar clientes:
+export const DELETE_CUSTOMER = "DELETE_CUSTOMER";
