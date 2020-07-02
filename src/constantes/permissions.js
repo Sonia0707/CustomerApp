5e49cd6f0371bec59726cc9cc3892ad71806b5fa
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-07-02 13:17:26 +0200

    constantes de los permisos y el archivo db.json

---
+++ src/constantes/permissions.js
@@ -0,0 +1,5 @@
+//Constantes para validar la seguridad de nuestra aplicación:
+//(Distintos permisos se acceso):
+export const CUSTUMER_LIST = "CUSTUMER_LIST";
+export const CUSTUMER_EDIT = "CUSTUMER_EDIT";
+export const CUSTUMER_VIEW = "CUSTUMER_VIEW";
