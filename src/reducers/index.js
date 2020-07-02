183f40c363c621cc2a98e53b1ed0db0787196061
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-07-02 13:16:53 +0200

    CUSTUMER_EDIT, CUSTUMER_LIST, CUSTUMER_VIEW => Permisos que se cumplen (esto hay que remplazarlo por un login form, actions creator, reducer y navegation) pero es la prueba para ver que los permisos funcionan.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,22 @@ import { combineReducers } from "redux";
 import { customers } from "./customers"; //Importamos el reducers.
 //Creamos el reducer de form: (Alias para que no sea un nombre muy generico)
 import { reducer as reduxForm } from "redux-form";
+//PERMISOS NOSOTROS DECIDIMOS QUE PERMISOS TIENE:
+import {
+  CUSTUMER_EDIT,
+  CUSTUMER_LIST,
+  CUSTUMER_VIEW,
+} from "./../constantes/permissions";
+
+//Generamos un reducers sencillo para la prueba de acceso (datos precargados de un usuario):
+const user = (state, action) => ({
+  permissions: [CUSTUMER_LIST, CUSTUMER_VIEW, CUSTUMER_EDIT],
+});
 
 //Creamos la llamada a los reducers con combineReducers:
 //Ponemos la kit de redux-form que es la que esta esperando: form: (es la recomendada)
 export default combineReducers({
   customers,
   form: reduxForm,
+  user,
 });
